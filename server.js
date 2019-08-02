8a496d6c2ff5f1845e48670db52f3604d3875f0c
Asmaathabet <asmathabet200@hotmail.com>
2019-08-02 20:58:18 +0300

    communicating with server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
-const server = http.createServer();
+
+//make router to communicate with server 
+const router = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('The router is connected successfully with server ');
+    response.end();
+}
+
+const server = http.createServer(router);
 
 const port = 5000;
-
 // run server by [node server.js] 
 server.listen(port, () => {
     console.log(`you can access website by http://localhost:${port}`)
